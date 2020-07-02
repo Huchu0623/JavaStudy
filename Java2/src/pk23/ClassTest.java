ddf5d1a92289b90e121d43b666e7e724be17707f
SOLDESK <SOLDESK@DESKTOP-GU12753>
2020-07-02 11:54:22 +0900

    20200702 pk23

---
+++ Java2/src/pk23/ClassTest.java
@@ -0,0 +1,13 @@
+package pk23;
+
+public class ClassTest {
+
+	public static void main(String[] args) {
+		
+		Person person = new Person();
+		Class pClass1 = person.getClass();
+		System.out.println(pClass1.getName());
+
+	}
+
+}
